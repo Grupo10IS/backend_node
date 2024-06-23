c1000de5881ca7fe814ebc01efec45bbccd39dbe
Elias Gill <eliasgill42@gmail.com>
2024-06-23 12:31:58 -0400

    docs: Adding swagger comments for reservations endpoint

--- src/routes/ReservationRouter.ts
+++ src/routes/ReservationRouter.ts
@@ -4,6 +4,81 @@ import { ReservationController } from "../controllers/ReservationController";
 export function NewReservationRouter(): Router {
     const reservationRouter: Router = express.Router();
     reservationRouter
+        /**
+         * @swagger
+         * /api/reservations:
+         *   get:
+         *     summary: Obtener lista de reservas
+         *     description: Retorna una lista de reservas filtrada opcionalmente por ID de restaurante, cliente y fecha.
+         *     tags: [Reservations]
+         *     parameters:
+         *       - in: query
+         *         name: resId
+         *         schema:
+         *           type: string
+         *         description: ID del restaurante
+         *       - in: query
+         *         name: client
+         *         schema:
+         *           type: string
+         *         description: ID del cliente
+         *       - in: query
+         *         name: date
+         *         schema:
+         *           type: string
+         *           format: date
+         *         description: Fecha de la reserva
+         *     responses:
+         *       200:
+         *         description: Lista de reservas encontradas
+         *         content:
+         *           application/json:
+         *             schema:
+         *               type: array
+         *               items:
+         *                 $ref: '#/components/schemas/Reservation'
+         *       400:
+         *         description: Error en el formato de la solicitud
+         */
+
+        /**
+         * @swagger
+         * components:
+         *   schemas:
+         *     Reservation:
+         *       type: object
+         *       required:
+         *         - id
+         *         - resId
+         *         - client
+         *         - date
+         *         - reservation_start
+         *         - reservation_end
+         *         - table
+         *       properties:
+         *         id:
+         *           type: integer
+         *           description: ID auto-generado de la reserva
+         *         resId:
+         *           type: integer
+         *           description: ID del restaurante
+         *         client:
+         *           type: integer
+         *           description: ID del cliente
+         *         date:
+         *           type: string
+         *           format: date
+         *           description: Fecha de la reserva
+         *         reservation_start:
+         *           type: integer
+         *           description: Hora de inicio de la reserva (en formato de 24 horas)
+         *         reservation_end:
+         *           type: integer
+         *           description: Hora de fin de la reserva (en formato de 24 horas)
+         *         table:
+         *           type: integer
+         *           description: ID de la mesa reservada
+         */
         .get("/", async function (req, res, next) {
             try {
                 const list = await ReservationController.listReservations({
@@ -19,6 +94,50 @@ export function NewReservationRouter(): Router {
             }
         })
 
+        /**
+         * @swagger
+         * /api/reservations:
+         *   post:
+         *     summary: Crear nueva reserva
+         *     description: Crea una nueva reserva con los detalles proporcionados.
+         *     tags: [Reservations]
+         *     requestBody:
+         *       required: true
+         *       content:
+         *         application/json:
+         *           schema:
+         *             type: object
+         *             required:
+         *               - clientDni
+         *               - tableId
+         *               - date
+         *               - reservationStart
+         *               - reservationEnd
+         *             properties:
+         *               clientDni:
+         *                 type: integer
+         *                 description: DNI del cliente
+         *               tableId:
+         *                 type: integer
+         *                 description: ID de la mesa reservada
+         *               date:
+         *                 type: string
+         *                 format: date
+         *                 description: Fecha de la reserva
+         *               reservationStart:
+         *                 type: integer
+         *                 description: Hora de inicio de la reserva (en formato de 24 horas)
+         *               reservationEnd:
+         *                 type: integer
+         *                 description: Hora de fin de la reserva (en formato de 24 horas)
+         *     responses:
+         *       200:
+         *         description: Reserva creada exitosamente
+         *       400:
+         *         description: Error en el formato de la solicitud o campos faltantes
+         *       409:
+         *         description: No se puede realizar la reservación
+         */
         .post("/", async function (req, res, next) {
             const body = req.body;
             // hacer la comprobacion de campos vacios
@@ -51,6 +170,31 @@ export function NewReservationRouter(): Router {
                 });
         })
 
+        /**
+         * @swagger
+         * /api/reservations:
+         *   delete:
+         *     summary: Cancelar una reserva
+         *     description: Cancela una reserva existente basada en su ID.
+         *     tags: [Reservations]
+         *     requestBody:
+         *       required: true
+         *       content:
+         *         application/json:
+         *           schema:
+         *             type: object
+         *             required:
+         *               - id
+         *             properties:
+         *               id:
+         *                 type: integer
+         *                 description: ID de la reserva a cancelar
+         *     responses:
+         *       200:
+         *         description: Reserva cancelada exitosamente
+         *       400:
+         *         description: Error en el formato de la solicitud o ID faltante
+         */
         .delete("/", async function (req, res, next) {
             if (!req.body.id || req.body.id === undefined) {
                 res.status(400).send("Se debe proporcionar el ID");
--- src/routes/TablesRouter.ts
+++ src/routes/TablesRouter.ts
@@ -4,6 +4,78 @@ import { TableController } from "../controllers/TableController";
 export function NewTablesRouter(): Router {
     const tablesRouter: Router = express.Router();
     tablesRouter
+        /**
+         * @swagger
+         * /api/tables:
+         *   get:
+         *     summary: Obtener lista de mesas
+         *     description: Retorna una lista de mesas filtrada opcionalmente por ID de restaurante, piso y capacidad.
+         *     tags: [Tables]
+         *     parameters:
+         *       - in: query
+         *         name: resId
+         *         schema:
+         *           type: string
+         *         description: ID del restaurante al que pertenecen las mesas
+         *       - in: query
+         *         name: floor
+         *         schema:
+         *           type: string
+         *         description: Número de piso de las mesas
+         *       - in: query
+         *         name: capacity
+         *         schema:
+         *           type: string
+         *         description: Capacidad mínima de las mesas
+         *     responses:
+         *       200:
+         *         description: Lista de mesas encontradas
+         *         content:
+         *           application/json:
+         *             schema:
+         *               type: array
+         *               items:
+         *                 $ref: '#/components/schemas/Table'
+         *       400:
+         *         description: Error en el formato de la solicitud
+         */
+
+        /**
+         * @swagger
+         * components:
+         *   schemas:
+         *     Table:
+         *       type: object
+         *       required:
+         *         - id
+         *         - name
+         *         - posX
+         *         - posY
+         *         - floor
+         *         - capacity
+         *       properties:
+         *         id:
+         *           type: integer
+         *           description: ID auto-generado de la mesa
+         *         resId:
+         *           type: integer
+         *           description: ID del restaurante al que pertenece la mesa
+         *         name:
+         *           type: string
+         *           description: Nombre de la mesa
+         *         posX:
+         *           type: integer
+         *           description: Posición X de la mesa
+         *         posY:
+         *           type: integer
+         *           description: Posición Y de la mesa
+         *         floor:
+         *           type: integer
+         *           description: Número de piso de la mesa
+         *         capacity:
+         *           type: integer
+         *           description: Capacidad de la mesa
+         */
         .get("/", async function (req, res, next) {
             try {
                 const list = await TableController.listAll({
@@ -19,6 +91,69 @@ export function NewTablesRouter(): Router {
             }
         })
 
+        /**
+         * @swagger
+         * /api/tables/{id}:
+         *   get:
+         *     summary: Obtener mesa por ID
+         *     description: Retorna los detalles de una mesa específica basada en su ID.
+         *     tags: [Tables]
+         *     parameters:
+         *       - in: path
+         *         name: id
+         *         required: true
+         *         schema:
+         *           type: integer
+         *         description: ID de la mesa a buscar
+         *     responses:
+         *       200:
+         *         description: Mesa encontrada
+         *         content:
+         *           application/json:
+         *             schema:
+         *               $ref: '#/components/schemas/Table'
+         *       404:
+         *         description: Mesa no encontrada
+         *       400:
+         *         description: Error en el formato de la solicitud
+         */
+
+        /**
+         * @swagger
+         * components:
+         *   schemas:
+         *     Table:
+         *       type: object
+         *       required:
+         *         - id
+         *         - name
+         *         - posX
+         *         - posY
+         *         - floor
+         *         - capacity
+         *       properties:
+         *         id:
+         *           type: integer
+         *           description: ID auto-generado de la mesa
+         *         resId:
+         *           type: integer
+         *           description: ID del restaurante al que pertenece la mesa
+         *         name:
+         *           type: string
+         *           description: Nombre de la mesa
+         *         posX:
+         *           type: integer
+         *           description: Posición X de la mesa
+         *         posY:
+         *           type: integer
+         *           description: Posición Y de la mesa
+         *         floor:
+         *           type: integer
+         *           description: Número de piso de la mesa
+         *         capacity:
+         *           type: integer
+         *           description: Capacidad de la mesa
+         */
         .get("/:id", async function (req, res, next) {
             try {
                 const id = req.params.id;
@@ -35,6 +170,90 @@ export function NewTablesRouter(): Router {
             }
         })
 
+        /**
+         * @swagger
+         * /api/tables:
+         *   post:
+         *     summary: Crear nueva mesa
+         *     description: Crea una nueva mesa con los detalles proporcionados.
+         *     tags: [Tables]
+         *     requestBody:
+         *       required: true
+         *       content:
+         *         application/json:
+         *           schema:
+         *             type: object
+         *             required:
+         *               - name
+         *               - resId
+         *               - posX
+         *               - posY
+         *               - floor
+         *               - capacity
+         *             properties:
+         *               name:
+         *                 type: string
+         *                 description: Nombre de la mesa
+         *               resId:
+         *                 type: integer
+         *                 description: ID del restaurante al que pertenece la mesa
+         *               posX:
+         *                 type: integer
+         *                 description: Posición X de la mesa
+         *               posY:
+         *                 type: integer
+         *                 description: Posición Y de la mesa
+         *               floor:
+         *                 type: integer
+         *                 description: Número de piso de la mesa
+         *               capacity:
+         *                 type: integer
+         *                 description: Capacidad de la mesa
+         *     responses:
+         *       200:
+         *         description: Mesa creada exitosamente
+         *       400:
+         *         description: Error en el formato de la solicitud o campos faltantes
+         *       409:
+         *         description: Ya existe una mesa en esa posición
+         */
+
+        /**
+         * @swagger
+         * components:
+         *   schemas:
+         *     Table:
+         *       type: object
+         *       required:
+         *         - id
+         *         - name
+         *         - posX
+         *         - posY
+         *         - floor
+         *         - capacity
+         *       properties:
+         *         id:
+         *           type: integer
+         *           description: ID auto-generado de la mesa
+         *         resId:
+         *           type: integer
+         *           description: ID del restaurante al que pertenece la mesa
+         *         name:
+         *           type: string
+         *           description: Nombre de la mesa
+         *         posX:
+         *           type: integer
+         *           description: Posición X de la mesa
+         *         posY:
+         *           type: integer
+         *           description: Posición Y de la mesa
+         *         floor:
+         *           type: integer
+         *           description: Número de piso de la mesa
+         *         capacity:
+         *           type: integer
+         *           description: Capacidad de la mesa
+         */
         .post("/", async function (req, res, next) {
             const body = req.body;
             // hacer la comprobacion de campos vacios
@@ -68,6 +287,88 @@ export function NewTablesRouter(): Router {
                 });
         })
 
+        /**
+         * @swagger
+         * /api/tables:
+         *   put:
+         *     summary: Actualizar mesa existente
+         *     description: Actualiza los detalles de una mesa existente basada en su ID.
+         *     tags: [Tables]
+         *     requestBody:
+         *       required: true
+         *       content:
+         *         application/json:
+         *           schema:
+         *             type: object
+         *             required:
+         *               - id
+         *               - name
+         *               - posX
+         *               - posY
+         *               - floor
+         *               - capacity
+         *             properties:
+         *               id:
+         *                 type: integer
+         *                 description: ID de la mesa a actualizar
+         *               name:
+         *                 type: string
+         *                 description: Nuevo nombre de la mesa
+         *               posX:
+         *                 type: integer
+         *                 description: Nueva posición X de la mesa
+         *               posY:
+         *                 type: integer
+         *                 description: Nueva posición Y de la mesa
+         *               floor:
+         *                 type: integer
+         *                 description: Nuevo número de piso de la mesa
+         *               capacity:
+         *                 type: integer
+         *                 description: Nueva capacidad de la mesa
+         *     responses:
+         *       200:
+         *         description: Mesa actualizada exitosamente
+         *       400:
+         *         description: Error en el formato de la solicitud o campos faltantes
+         */
+
+        /**
+         * @swagger
+         * components:
+         *   schemas:
+         *     Table:
+         *       type: object
+         *       required:
+         *         - id
+         *         - name
+         *         - posX
+         *         - posY
+         *         - floor
+         *         - capacity
+         *       properties:
+         *         id:
+         *           type: integer
+         *           description: ID auto-generado de la mesa
+         *         resId:
+         *           type: integer
+         *           description: ID del restaurante al que pertenece la mesa
+         *         name:
+         *           type: string
+         *           description: Nombre de la mesa
+         *         posX:
+         *           type: integer
+         *           description: Posición X de la mesa
+         *         posY:
+         *           type: integer
+         *           description: Posición Y de la mesa
+         *         floor:
+         *           type: integer
+         *           description: Número de piso de la mesa
+         *         capacity:
+         *           type: integer
+         *           description: Capacidad de la mesa
+         */
         .put("/", async function (req, res, next) {
             const body = req.body;
 
@@ -87,6 +388,68 @@ export function NewTablesRouter(): Router {
                 });
         })
 
+        /**
+         * @swagger
+         * /api/tables:
+         *   delete:
+         *     summary: Eliminar mesa por ID
+         *     description: Elimina una mesa existente basada en su ID.
+         *     tags: [Tables]
+         *     requestBody:
+         *       required: true
+         *       content:
+         *         application/json:
+         *           schema:
+         *             type: object
+         *             required:
+         *               - id
+         *             properties:
+         *               id:
+         *                 type: integer
+         *                 description: ID de la mesa a eliminar
+         *     responses:
+         *       200:
+         *         description: Mesa eliminada exitosamente
+         *       400:
+         *         description: Error en el formato de la solicitud o ID faltante
+         */
+
+        /**
+         * @swagger
+         * components:
+         *   schemas:
+         *     Table:
+         *       type: object
+         *       required:
+         *         - id
+         *         - name
+         *         - posX
+         *         - posY
+         *         - floor
+         *         - capacity
+         *       properties:
+         *         id:
+         *           type: integer
+         *           description: ID auto-generado de la mesa
+         *         resId:
+         *           type: integer
+         *           description: ID del restaurante al que pertenece la mesa
+         *         name:
+         *           type: string
+         *           description: Nombre de la mesa
+         *         posX:
+         *           type: integer
+         *           description: Posición X de la mesa
+         *         posY:
+         *           type: integer
+         *           description: Posición Y de la mesa
+         *         floor:
+         *           type: integer
+         *           description: Número de piso de la mesa
+         *         capacity:
+         *           type: integer
+         *           description: Capacidad de la mesa
+         */
         .delete("/", async function (req, res, next) {
             if (!req.body.id || req.body.id === undefined) {
                 res.status(400).send("Se debe proporcionar el ID");
